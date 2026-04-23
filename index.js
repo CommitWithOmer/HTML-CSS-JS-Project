5b52200021d0bdd10f0d39ae800bacc03fdafa3d
Syed Omer Hussaini <syedomerhussaini@outlook.com>
2026-04-23 17:17:16 +0530

    Third Commit

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ document.querySelectorAll('.dropdown-toggle').forEach(btn => {
     btn.parentElement.classList.toggle('active');
   };
 });
+
+
+const menu = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav-links');
+
+menu.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
